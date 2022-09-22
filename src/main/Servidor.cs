862875afc44859a702fd73c93f50cc24668ba537
José Alberto Rosales Peña <joserosales@ciencias.unam.mx>
2022-09-22 12:57:39 -0500

    cliente

--- src/main/Servidor.cs
+++ src/main/Servidor.cs
@@ -7,7 +7,14 @@ namespace Chat {
     public class Servidor  
     {  
         
-  
+  	public class main
+	    {          
+        public static void Main(string[] args)
+        {   
+
+            Inicia();      
+        }
+    }
         public static void Inicia()  
         {  
             IPHostEntry host = Dns.GetHostEntry("localhost");  
@@ -22,15 +29,24 @@ namespace Chat {
   
                 Console.WriteLine("Esperando conexión...");  
                 Socket cliente = servidor.Accept();  
-  
-                string data = null;  
-                byte[] bytes = null;  
-  
 
+
+                string data = null;  
+                byte[] bytes = null;
+  
+  
+            
+                bytes = new byte[1024];  
+                int bytesRec = cliente.Receive(bytes);  
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);  
+
+            
   
                 Console.WriteLine("Cliente recibido");  
   
-
+                byte[] mensaje = Encoding.ASCII.GetBytes(data + "mundo");  
+                cliente.Send(mensaje);  
+                cliente.Shutdown(SocketShutdown.Both);  
                 cliente.Close();  
             }  
             catch (Exception e)  
